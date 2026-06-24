f935b61579459a82ff7111be8e58781fd25eacc6
00Swendy00 <121340555+00Swendy00@users.noreply.github.com>
2026-06-24 20:47:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-window.onload = function () {
+// LOADER
+
+window.addEventListener("load", () => {
 
     const loader = document.getElementById("loader");
 
-    if (loader) {
-        loader.style.display = "none";
+    if(loader){
+
+        setTimeout(() => {
+
+            loader.style.opacity = "0";
+
+            setTimeout(() => {
+
+                loader.style.display = "none";
+
+            }, 500);
+
+        }, 1000);
+
     }
 
-};
+});
+
+// CURSOR GLOW
+
+const glow = document.querySelector(".cursor-glow");
+
+if(glow){
+
+    document.addEventListener("mousemove", (e) => {
+
+        glow.style.left = e.clientX + "px";
+        glow.style.top = e.clientY + "px";
+
+    });
+
+}
