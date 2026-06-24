de89a1c301ac91f6cd8cdb5eeb0ee990b9ca0a5d
00Swendy00 <121340555+00Swendy00@users.noreply.github.com>
2026-06-24 21:22:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (!glow) return;
 
     document.addEventListener("mousemove", (e) => {
-        glow.style.left = `${e.clientX}px`;
+       glow.style.left = `${e.clientX}px`;
         glow.style.top = `${e.clientY}px`;
     });
 });
